7c0e01776d64a470bd47b8b0fdf73b7f1234c69e
cameronnapoli <cameronnapoli@gmail.com>
2018-12-05 14:06:40 -0800

    Working skeleton for API completed.

--- api/main.go
+++ api/main.go
@@ -1,9 +1,13 @@
 package main
 
+// https://github.com/danhenriquesc/go-poll
+
 import (
     "fmt"
-    "encoding/json"
-    // "github.com/gorilla/mux"
+    // "encoding/json"
+    "net/http"
+    "log"
+    "github.com/gorilla/mux"
     // "github.com/go-redis/redis"
 )
 
@@ -34,20 +38,41 @@ Display how many tickets remain (open, purchasing, purchased)
 
 */
 
-func
+func GetRemainingTickets(w http.ResponseWriter, r *http.Request) {
+    fmt.Println("* GetRemainingTickets")
+
+    w.WriteHeader(http.StatusOK)
+    fmt.Fprintf(w, "<p>Html content.</p>")
+}
+
+func LockTicket(w http.ResponseWriter, r *http.Request) {
+    fmt.Println("* LockTicket")
+}
+
+func BuyTicket(w http.ResponseWriter, r *http.Request) {
+    fmt.Println("* BuyTicket")
+}
 
 
 func main() {
-    fmt.Println("Starting application...")
+    router := mux.NewRouter()
 
-    tr := &TicketResponse{UserToken: "0f8238n2fn803f2",
-                          TicketType: TICKET_GA,
-                          PaymentToken: "82748719712"}
+    router.HandleFunc("/remaining_tickets", GetRemainingTickets).Methods("GET")
+    router.HandleFunc("/buy_ticket", LockTicket).Methods("POST")
+    router.HandleFunc("/buy_ticket/{token}", BuyTicket).Methods("POST")
 
-    b, err := json.Marshal(tr)
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
-    fmt.Println(string(b))
+    fmt.Println("Listening on port 8000.")
+    log.Fatal(http.ListenAndServe(":8000", router))
+
+
+    // tr := &TicketResponse{UserToken: "0f8238n2fn803f2",
+    //                       TicketType: TICKET_GA,
+    //                       PaymentToken: "82748719712"}
+    //
+    // b, err := json.Marshal(tr)
+    // if err != nil {
+    //     fmt.Println(err)
+    //     return
+    // }
+    // fmt.Println(string(b))
 }
